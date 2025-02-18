9b6535a5044d3edbeccb86fc692806236dfc0e13
jinixa8 <banugaws@gmail.com>
2025-02-17 22:43:12 -0500

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Jinixa team with Boo, Ivor, Amit, Nay, Jdawg, Dony, Xoxo.");
-      System.out.println("Hello Squad Jinixa8, we are GoD-fearing Powerful and beautiful famiglia.");
+     System.out.println("Hello Jinixa team with Boo, Ivor, Nay, Jdawg, Dony, Xoxo.");
+      System.out.println("Hello Squad Jinixa8, we are a God-Fearing Powerful and beautiful famiglia.");
     }  
 } 
